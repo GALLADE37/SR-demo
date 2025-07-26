09a64d4d1b5d2e526fd80a0ab4e276a086c429bc
GALLADE37 <134584318+GALLADE37@users.noreply.github.com>
2025-07-26 19:01:26 +0900

    Add files via upload

---
+++ init_srcnn.py
@@ -0,0 +1,222 @@
+import numpy as np
+import os
+from PIL import Image, ImageOps, ImageFile
+import random
+import torch
+from torch.utils.data import Dataset
+import random
+import torchvision.transforms.functional as TF
+from torchvision.transforms import v2
+import sys
+sys.setrecursionlimit(10000)
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+class PairedRandomFlipRotate:
+    """对两张图像同时进行随机水平/垂直翻转和旋转，支持 Compose 使用"""
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, img1, img2):
+        # 水平翻转
+        if random.random() < self.p:
+            img1 = TF.hflip(img1)
+            img2 = TF.hflip(img2)
+
+        # 垂直翻转
+        if random.random() < self.p:
+            img1 = TF.vflip(img1)
+            img2 = TF.vflip(img2)
+
+        # 随机旋转
+        if random.random() < self.p:
+            angle = random.choice([90, 180, 270])
+            img1 = TF.rotate(img1, angle)
+            img2 = TF.rotate(img2, angle)
+
+        return img1, img2
+
+
+def calc_psnr(img1, img2):
+    return 10. * torch.log10(1. / torch.mean((img1 - img2) ** 2))
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = self.avg = self.sum = self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def random_crop_img(img, patch_size):
+    if (img.size[0] < patch_size):
+        img = ImageOps.crop(img, border=img.size[0] - patch_size)
+    if (img.size[1] < patch_size):
+        img = ImageOps.crop(img, border=img.size[1] - patch_size)
+    sp1, sp2 = random.randint(0, img.size[0] - patch_size), random.randint(0, img.size[1] - patch_size)
+    return img.crop((sp1, sp2, sp1 + patch_size, sp2 + patch_size))
+
+def make_LR_patch(img, upscale_factor, withupsampling):
+    H, W = img.size
+    if withupsampling == True:
+        img0 = img.resize((H//upscale_factor, W//upscale_factor), Image.Resampling.BICUBIC)
+        return img0.resize((H, W), Image.Resampling.BICUBIC)
+    else:
+        return img.resize((H//upscale_factor, W//upscale_factor), Image.Resampling.BICUBIC)
+
+def data_initialization(img):
+    return np.expand_dims(np.asarray(img, np.float32), axis=0) / 255.
+
+def get_all_tif_files(directory):
+    tif_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith('.tif'):
+                tif_files.append(os.path.join(root, file))
+    return tif_files
+
+def get_all_png_files(directory):
+    png_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith('.png'):
+                png_files.append(os.path.join(root, file))
+    return png_files
+
+def get_all_jpg_files(directory):
+    jpg_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith('.jpg'):
+                jpg_files.append(os.path.join(root, file))
+    return jpg_files
+
+class TrainDataset(Dataset):
+    def __init__(self, imgpath, patch_size=None, upscale_factor=None, upsampling=False):
+        super(TrainDataset, self).__init__()
+        self.HR_img_list = get_all_jpg_files(imgpath)
+        self.upscale_factor = upscale_factor
+        self.withupsampling = upsampling
+        self.patch_size = patch_size
+        self.augment = PairedRandomFlipRotate(p=0.5)
+
+    def __getitem__(self, index):
+        HR_img = Image.open(self.HR_img_list[index]).convert('L')
+
+        sp1, sp2 = (
+            random.randint(0, HR_img.size[0] - self.patch_size), random.randint(0, HR_img.size[1] - self.patch_size))
+
+        HR_patch = HR_img.crop((sp1, sp2, sp1 + self.patch_size, sp2 + self.patch_size))
+        LR_patch = make_LR_patch(HR_patch, self.upscale_factor, self.withupsampling)
+
+        LR_patch, HR_patch = torch.as_tensor(data_initialization(LR_patch)), torch.as_tensor(data_initialization(HR_patch))
+
+        return self.augment(LR_patch, HR_patch)
+
+    def __len__(self):
+        return len(self.HR_img_list)
+
+
+class EvalDataset(Dataset):
+    def __init__(self, imgpath, patch_size=None, upscale_factor=None, upsampling=False):
+        super(EvalDataset, self).__init__()
+        self.HR_img_list = get_all_jpg_files(imgpath)
+        self.upscale_factor = upscale_factor
+        self.withupsampling = upsampling
+        self.patch_size = patch_size
+        # self.augment = v2.Compose([
+        #     v2.RandomCrop(size=[patch_size, patch_size]),
+        #     v2.RandomHorizontalFlip(p=0.5),
+        #     v2.RandomVerticalFlip(p=0.5)
+        # ])
+
+    def __getitem__(self, index):
+        HR_img = Image.open(self.HR_img_list[index]).convert('L')
+
+        sp1, sp2 = (
+            random.randint(0, HR_img.size[0] - self.patch_size), random.randint(0, HR_img.size[1] - self.patch_size))
+
+        HR_patch = HR_img.crop((sp1, sp2, sp1 + self.patch_size, sp2 + self.patch_size))
+        LR_patch = make_LR_patch(HR_patch, self.upscale_factor, self.withupsampling)
+
+        return torch.as_tensor(data_initialization(LR_patch)), torch.as_tensor(data_initialization(HR_patch))
+
+    def __len__(self):
+        return len(self.HR_img_list)
+
+# class TestDataset(Dataset):
+#     def __init__(self, imgpath, patch_size=None, upscale_factor=None, upsampling=False):
+#         super(TestDataset, self).__init__()
+#         self.imgpath = imgpath
+#         self.patch_size = patch_size
+#         self.upscale_factor = upscale_factor
+#         self.withupsampling = upsampling
+#         self.HR_img_list = get_all_tif_files(self.imgpath)
+#
+#     def __getitem__(self, index):
+#         HR_img = Image.open(self.HR_img_list[index]).convert('L')
+#
+#         if self.patch_size:
+#             if (HR_img.size[0] < self.patch_size):
+#                 HR_img = ImageOps.crop(HR_img, border=HR_img.size[0] - self.patch_size)
+#             if (HR_img.size[1] < self.patch_size):
+#                 HR_img = ImageOps.crop(HR_img, border=HR_img.size[1] - self.patch_size)
+#
+#             sp1, sp2 = HR_img.size[0] // 2 - self.patch_size // 2, HR_img.size[1] // 2 - self.patch_size // 2
+#             HR_patch = HR_img.crop((sp1, sp2, sp1 + self.patch_size, sp2 + self.patch_size))
+#
+#         else:
+#             HR_patch = HR_img.crop((0, 0, HR_img.size[0] - HR_img.size[0] % self.upscale_factor,
+#                                     HR_img.size[1] - HR_img.size[1] % self.upscale_factor))
+#
+#         LR_patch = make_LR_patch(HR_patch, self.upscale_factor, self.withupsampling)
+#
+#         return torch.as_tensor(data_initialization(LR_patch)), torch.as_tensor(data_initialization(HR_patch))
+#
+#     def __len__(self):
+#         return len(os.listdir(self.imgpath))
+#
+# class TestDataset_temp(Dataset):
+#     def __init__(self, imgpath, patch_size=None, upscale_factor=None, upsampling=False):
+#         super(TestDataset_temp, self).__init__()
+#         self.imgpath = imgpath
+#         self.patch_size = patch_size
+#         self.upscale_factor = upscale_factor
+#         self.withupsampling = upsampling
+#         self.HR_img_list = get_all_tif_files(self.imgpath)
+#
+#     def __getitem__(self, index):
+#         HR_img = Image.open(self.HR_img_list[index]).convert('L')
+#
+#         if self.patch_size:
+#             if (HR_img.size[0] < self.patch_size):
+#                 HR_img = ImageOps.crop(HR_img, border=HR_img.size[0] - self.patch_size)
+#             if (HR_img.size[1] < self.patch_size):
+#                 HR_img = ImageOps.crop(HR_img, border=HR_img.size[1] - self.patch_size)
+#
+#             sp1, sp2 = HR_img.size[0] // 2 - self.patch_size // 2, HR_img.size[1] // 2 - self.patch_size // 2
+#             HR_patch = HR_img.crop((sp1, sp2, sp1 + self.patch_size, sp2 + self.patch_size))
+#
+#         else:
+#             HR_patch = HR_img.crop((0, 0, HR_img.size[0] - HR_img.size[0] % self.upscale_factor,
+#                                     HR_img.size[1] - HR_img.size[1] % self.upscale_factor))
+#
+#         LR_patch = make_LR_patch(HR_patch, self.upscale_factor, self.withupsampling)
+#
+#         LR_patch = torch.as_tensor(data_initialization(LR_patch))
+#         HR_patch = torch.as_tensor(data_initialization(HR_patch))
+#
+#         LR_patch = torch.cat((LR_patch[:,:32,:], LR_patch[:,32:,:]), dim=-1)
+#         HR_patch = torch.cat((HR_patch[:,:128,:], HR_patch[:,128:,:]), dim=-1)
+#
+#         return torch.cat((LR_patch, LR_patch, LR_patch, LR_patch), dim=1), torch.cat((HR_patch, HR_patch, HR_patch, HR_patch), dim=1)
+#
+#     def __len__(self):
+#         return len(os.listdir(self.imgpath))
---
+++ train_SRCNN.py
@@ -0,0 +1,120 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import argparse
+import torch
+from torch import autocast
+from torch.cuda.amp import GradScaler
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torch.backends.cudnn as cudnn
+from tqdm import tqdm
+
+from init_srcnn import TrainDataset, EvalDataset, calc_psnr, AverageMeter
+from model.SRCNN import SRCNN
+
+# eval:
+# 2x: 33.26
+# 4x: 27.26
+
+def train():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_dir', type=str, default='/media/gallade/disk2/AID/train')
+    parser.add_argument('--valid_dir', type=str, default='/media/gallade/disk2/AID/valid')
+    parser.add_argument('--save_weight_dir', type=str, default='SRCNNx4.pth')  ###
+    parser.add_argument('--upscale_factor', type=int, default=4)  ###
+    parser.add_argument('--preupsampling', type=bool, default=True)  ###
+    parser.add_argument('--lr', type=float, default=5e-4)  # SRCNN:5e-4  RCAN:1e-4
+    parser.add_argument('--train_patch_size', type=int, default=32) # x2:32  x4:64  x8:128
+    parser.add_argument('--valid_patch_size', type=int, default=256)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--num-epochs', type=int, default=1000)
+    parser.add_argument('--num_workers', type=int, default=32)
+    parser.add_argument('--thread', type=int, default=32)
+    parser.add_argument('--seed', type=int, default=123)
+    args = parser.parse_args()
+    torch.manual_seed(args.seed)
+    torch.set_num_threads(args.thread)
+    cudnn.benchmark = True
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    model = SRCNN().to(device)
+    # model.load_state_dict(torch.load("SRCNNx2.pth"))
+
+    criterion = nn.L1Loss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    scaler = GradScaler()
+
+    train_dataset = TrainDataset(args.train_dir, args.train_patch_size, args.upscale_factor, args.preupsampling)
+    train_dataloader = DataLoader(dataset=train_dataset,
+                                  batch_size=args.batch_size,
+                                  shuffle=True,
+                                  num_workers=args.num_workers,
+                                  drop_last=True,
+                                  pin_memory=True)
+    eval_dataset = EvalDataset(args.valid_dir, args.valid_patch_size, args.upscale_factor, args.preupsampling)
+    eval_dataloader = DataLoader(dataset=eval_dataset, shuffle=False, batch_size=16)
+
+    best_psnr = 0
+    no_improve_count = 0
+    early_stop_patience = 20
+
+    for epoch in range(args.num_epochs):
+        model.train()
+        epoch_losses = AverageMeter()
+
+        with tqdm(total=len(train_dataloader), desc=f'Train Epoch {epoch}/{args.num_epochs}') as t:
+            for data in train_dataloader:
+                inputs, labels = [d.to(device, non_blocking=True) for d in data]
+
+                with autocast(device_type='cuda', dtype=torch.float16):
+                    preds = model(inputs)
+
+                loss = criterion(preds, labels)
+                epoch_losses.update(loss.item(), len(inputs))
+
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+                optimizer.zero_grad(set_to_none=True)
+
+                t.set_postfix(loss=f'{epoch_losses.avg:.6f}')
+                t.update(1)
+
+        if epoch % 1 == 0:
+            model.eval()
+            epoch_psnr = AverageMeter()
+
+            with tqdm(total=len(eval_dataloader), desc=f'Valid Epoch {epoch}/{args.num_epochs}') as k:
+                for data in eval_dataloader:
+                    inputs, labels = [d.to(device, non_blocking=True) for d in data]
+
+                    with torch.no_grad():
+                        preds = model(inputs).clamp(0.0, 1.0)
+
+                    psnr = calc_psnr(preds, labels)
+                    epoch_psnr.update(psnr.item(), len(inputs))
+
+                    k.set_postfix(PSNR=f'{epoch_psnr.avg:.6f}')
+                    k.update(1)
+
+            # Save the best model weights
+            print(f'Eval PSNR: {epoch_psnr.avg:.2f}, Best PSNR: {best_psnr:.2f}')
+            if epoch_psnr.avg > best_psnr:
+                best_psnr = epoch_psnr.avg
+                torch.save(model.state_dict(), args.save_weight_dir)
+                print('/////////// The best weight was saved! ///////////')
+                no_improve_count = 0
+            else:
+                no_improve_count += 1
+                if no_improve_count >= early_stop_patience:
+                    print(
+                        f'Early stopping at epoch {epoch} due to no PSNR improvement for {early_stop_patience} epochs.')
+                    break
+
+        # Clean up
+        torch.cuda.empty_cache()
+
+if __name__ == '__main__':
+    train()
